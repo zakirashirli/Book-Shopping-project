4d22072573934e9c042295ebfd8cee1ba0ae9b39
zakir ashirli 17640 <asirlizakir@gmail.com>
2023-04-08 00:48:40 +0400

    FindById method

--- projects/bookshopping/src/main/java/az/developia/bookshopping/controller/BookrestController.java
+++ projects/bookshopping/src/main/java/az/developia/bookshopping/controller/BookrestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,13 @@ public class BookrestController {
 	public List<Book> findAll() {
 
 		return bookDAO.findAll();
+
+	}
+
+	@GetMapping(path = "/{id}")
+	public Book findById(@PathVariable(name = "id") Integer id) {
+
+		return bookDAO.findById(id).get();
 
 	}
 
