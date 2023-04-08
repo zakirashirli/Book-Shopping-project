f22bd2b261ccef5677810fb20510c44cf1e5e30c
zakir ashirli 17640 <asirlizakir@gmail.com>
2023-04-08 13:28:42 +0400

    Yeni Kitab Qeydiyyati

--- projects/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
+++ projects/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookshopping.dao.BookDAO;
 import az.developia.bookshopping.model.Book;
@@ -18,19 +20,36 @@ public class BookController {
 
 	@GetMapping(path = "/books")
 	public String showBooks(Model model) {
+
+		List<Book> books = bookDAO.findAll();
+
+		model.addAttribute("books", books);
+
+		return "books";
+
+	}
+
+	@GetMapping(path = "/books/new")
+	public String openNewBookPage(Model model) {
+
+		Book book = new Book();
+
+		model.addAttribute("book", book);
+
+		return "new-book";
+
+	}
+
+	@PostMapping(path = "/books/new-book-process")
+	public String saveBook(@ModelAttribute(name = "book") Book book, Model model) {
+
+		bookDAO.save(book);
 		
 		List<Book> books = bookDAO.findAll();
 		
 		model.addAttribute("books", books);
-		
-		return "books";
 
-	}
-	
-	@GetMapping(path = "/books/new")
-	public String openNewBookPage() {
-		
-		return "new-book";
+		return "books";
 
 	}
 
