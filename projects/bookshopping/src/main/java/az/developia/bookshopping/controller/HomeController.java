d01e8586b158bdf048a3a368dd99103a6beb5079
zakir ashirli 17640 <asirlizakir@gmail.com>
2023-04-08 14:34:46 +0400

    19.Linkler arasinda rahat kecid

--- projects/bookshopping/src/main/java/az/developia/bookshopping/controller/HomeController.java
+++ projects/bookshopping/src/main/java/az/developia/bookshopping/controller/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping(path = "/home")
+
+	@GetMapping(path = { "/home", "/" })
 	public String showHomePage() {
-		
+
 		return "home";
-		
+
 	}
-	
+
 }
