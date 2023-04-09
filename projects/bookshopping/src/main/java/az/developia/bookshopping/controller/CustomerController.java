ae57c46919b7488ed11d3444fde483ffdbee1562
zakir ashirli 17640 <asirlizakir@gmail.com>
2023-04-09 12:52:52 +0400

    32. Customer ve Orders shevelerinin yaradilasi

---
+++ projects/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package az.developia.bookshopping.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import az.developia.bookshopping.dao.BookDAO;
+import az.developia.bookshopping.model.Book;
+
+@Controller
+public class CustomerController {
+
+	@GetMapping(path = "/customer")
+	public String showCustomerPage() {
+
+		return "customer";
+
+	}
+
+}
